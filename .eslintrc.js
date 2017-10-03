dce163d812003fe69343b427a10489603e5fdb20
Sullivan SENECHAL <soullivaneuh@gmail.com>
2017-10-03 22:04:05 +0200

    js-fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es6': true,
+    'jquery': true,
+  },
   'extends': 'eslint:all',
+  'rules': {
+    'no-console': 'off',
+    'strict': 'off',
+  },
 };
--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-let test = "toto";
+const test = "toto";
+console.log(test);
